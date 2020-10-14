24d1afaa5747d4d22d8e3454b592c8498a46d3b9
yanal <yanal.fard@gmail.com>
2020-10-14 10:43:00 +0330

    Uk added, Scaffhold files added, polished.

--- AminWeb/Controllers/HomeController.cs
+++ AminWeb/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataLayer.Models;
 
 namespace AminWeb.Controllers
 {
@@ -13,18 +14,5 @@ namespace AminWeb.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
