9a13c9923496eb8fbbb47dbd51d7e4fc2811c974
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-11-05 17:33:31 +0330

    Profile, Change Password, Wallet

---
+++ AminWeb/Areas/User/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AminWeb.Areas.User.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: User/Account
+        public ActionResult UserData()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        public ActionResult Wallet()
+        {
+            return View();
+        }
+
+    }
+}
