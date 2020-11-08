d683423740a43a5ef3d28f79d424dcb8e5951aa8
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-11-08 17:34:08 +0330

    About Layout, Contact Layout, Profile View

---
+++ AminWeb/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AminWeb.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Videos()
+        {
+            return View();
+        }
+
+        public ActionResult Classes()
+        {
+            return View();
+        }
+
+        public ActionResult SearchResults()
+        {
+            return View();
+        }
+
+    }
+}
