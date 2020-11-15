a5926e3c1357c946cf8af6d3cc51366b9c3a55d0
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-11-15 16:33:32 +0330

    Admin Dashboard Layout

---
+++ AminWeb/Areas/Admin/Controllers/ChatController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AminWeb.Areas.Admin.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Admin/Chat
+        public ActionResult ChatList()
+        {
+            return View();
+        }
+
+        public ActionResult ChatWithUser()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ AminWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AminWeb.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult UserList()
+        {
+            return View();
+        }
+
+        public ActionResult ViewSingleUser()
+        {
+            return View();
+        }
+
+        public ActionResult EditUser()
+        {
+            return View();
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+    }
+}
